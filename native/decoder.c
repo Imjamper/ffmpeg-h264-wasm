1c97b6f32142041dd3981ed28ce773df45bbe8af
Гачегов Анатолий Владимирович <Imjamper@gmail.com>
2023-08-17 02:54:04 +0400

    fixes

--- native/decoder.c
+++ native/decoder.c
@@ -3,8 +3,7 @@
 #include "libavcodec/avcodec.h"
 #include "libavutil/imgutils.h"
 
-AVCodecContext *
-create_codec_context() {
+AVCodecContext * create_codec_context() {
     const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_H264);
     AVCodecContext *ctx = avcodec_alloc_context3(codec);
     if (avcodec_open2(ctx, codec, NULL) < 0) {
@@ -15,19 +14,15 @@ create_codec_context() {
     return ctx;
 }
 
-void
-destroy_codec_context(AVCodecContext *ctx) {
+void destroy_codec_context(AVCodecContext *ctx) {
     avcodec_free_context(&ctx);
 }
 
-void
-close_frame(AVFrame *frame) {
-    av_frame_unref(frame);
+void close_frame(AVFrame *frame) {
     av_frame_free(&frame);
 }
 
-AVFrame *
-decode(AVCodecContext *ctx,
+AVFrame * decode(AVCodecContext *ctx,
        uint8_t *data_in,
        int data_in_size,
        uint8_t **y_plane_out,
@@ -36,28 +31,44 @@ decode(AVCodecContext *ctx,
        int *width_out,
        int *height_out,
        int *stride_out,
+       unsigned long *timestamp_out,
        int *is_key_frame
 ) {
-    AVFrame *frame = av_frame_alloc();
-    AVPacket *avpkt = av_packet_alloc();
     int ret;
 
+    AVPacket *avpkt = av_packet_alloc();
     avpkt->data = data_in;
     avpkt->size = data_in_size;
 
+    AVDictionary *dict = NULL;
+    if (av_packet_unpack_dictionary(data_in, data_in_size, &dict) >= 0) {
+      AVDictionaryEntry *dataEntry = av_dict_get(dict, "d", NULL, 0);
+      if (dataEntry) {
+        avpkt->data = dataEntry->value;
+      }
+      AVDictionaryEntry *sizeEntry = av_dict_get(dict, "s", NULL, 0);
+      if (sizeEntry) {
+        avpkt->size = stoi(sizeEntry->value);
+      }
+      AVDictionaryEntry *timeEntry = av_dict_get(dict, "t", NULL, 0);
+      if (timeEntry) {
+        timestamp_out = strtoull(timeEntry->value, NULL, 10);
+      }
+      
+      av_dict_free(dict);
+    }
+
     ret = avcodec_send_packet(ctx, avpkt);
     if (ret != 0) {
-        // TODO handle error codes?
-		av_packet_unref(avpkt);
 		av_packet_free(&avpkt);
         return NULL;
     }
 
+    AVFrame *frame = av_frame_alloc();
     ret = avcodec_receive_frame(ctx, frame);
     if (ret != 0) {
-        // TODO handle error codes?
-		av_packet_unref(avpkt);
 		av_packet_free(&avpkt);
+        av_frame_free(&frame);
         return NULL;
     }
 
@@ -69,7 +80,6 @@ decode(AVCodecContext *ctx,
     *height_out = frame->height;
     *is_key_frame = frame->key_frame;
 	
-	av_packet_unref(avpkt);
 	av_packet_free(&avpkt);
 
     return frame;
